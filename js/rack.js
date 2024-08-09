a0793d0dafab6b655a2a24f6e236a93497aa32f4
ilhams-sudo <fikrieadjoe@gmail.com>
2024-08-10 01:38:07 +0700

    Create rack.js

---
+++ js/rack.js
@@ -0,0 +1,12 @@
+function getRack(rackPart) {
+    switch(rackPart) {
+        case "1":
+            return "A";
+        case "2":
+            return "B";
+        case "3":
+            return "C";
+        default:
+            return null;
+    }
+}
