13039b76e4365a6e5e93333408ea4373f00dabd7
ilhams-sudo <fikrieadjoe@gmail.com>
2024-08-10 08:17:29 +0700

    Update rack.js
    Add rack up to 33

--- js/rack.js
+++ js/rack.js
@@ -6,6 +6,66 @@ function getRack(rackPart) {
             return "B";
         case "3":
             return "C";
+        case "4":
+            return "D";
+        case "5":
+            return "E";
+        case "6":
+            return "F";
+        case "7":
+            return "G";
+        case "8":
+            return "H";
+        case "9":
+            return "I";
+        case "10":
+            return "J";
+        case "11":
+            return "K";
+        case "12":
+            return "L";
+        case "13":
+            return "M";
+        case "14":
+            return "N";
+        case "15":
+            return "O";
+        case "16":
+            return "P";
+        case "17":
+            return "Q";
+        case "18":
+            return "R";
+        case "19":
+            return "S";
+        case "20":
+            return "T";
+        case "21":
+            return "U";
+        case "22":
+            return "V";
+        case "23":
+            return "W";
+        case "24":
+            return "X";
+        case "25":
+            return "Y";
+        case "26":
+            return "Z";
+        case "27":
+            return "AA";
+        case "28":
+            return "AB";
+        case "29":
+            return "AC";
+        case "30":
+            return "AD";
+        case "31":
+            return "AE";
+        case "32":
+            return "AF";
+        case "33":
+            return "AG";
         default:
             return null;
     }
